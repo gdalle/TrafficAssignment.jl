1b7ffec92c564c1a542cba6fe34a77d97cb130ed
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-06-05 11:18:56 +0200

    Add Tyler

--- test/plot.jl
+++ test/plot.jl
@@ -1,12 +1,12 @@
 using TestItems
 
-@testset "Plotting" begin
-    pb = TrafficAssignmentProblem("TransportationNetworks", "SiouxFalls")
-    plot_network(pb, pb.optimal_flow; nodes=true)
-    if VERSION >= v"1.11"
-        using Pkg
-        Pkg.add("Tyler")
-        using Tyler
+@testitem "Plotting" begin
+    using CairoMakie, Tyler
+    @testset "Makie" begin
+        pb = TrafficAssignmentProblem("TransportationNetworks", "SiouxFalls")
+        plot_network(pb, pb.optimal_flow; nodes=true)
+    end
+    @testset "Tyler" begin
         pb = TrafficAssignmentProblem("UnifiedTrafficDataset", "San Francisco")
         plot_network(pb; tiles=true, zones=true)
     end
